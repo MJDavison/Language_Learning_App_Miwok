4538ac9e901b730f54621534cb5014e47d4028ee
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-07-02 21:30:15 +0100

    Added all the other words as well as deleting all the comments. I recall the tutorial mentioned adding more than what I have so far... maybe it's not over?

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.miwok;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class ColorsActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colors);
+
+        ArrayList<Word> words = new ArrayList<>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+
+        WordAdapter itemsAdapter = new WordAdapter(this, words);
+        ListView listView = findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -16,34 +16,6 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        /*
-        //Create an array of words
-        String[] words = new String[10];
-
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
-
-        Log.v(LOG_TAG, "Word at index 0: " + words[0]);
-        Log.v(LOG_TAG, "Word at index 1: " + words[1]);
-        Log.v(LOG_TAG, "Word at index 2: " + words[2]);
-        Log.v(LOG_TAG, "Word at index 3: " + words[3]);
-        Log.v(LOG_TAG, "Word at index 4: " + words[4]);
-        Log.v(LOG_TAG, "Word at index 5: " + words[5]);
-        Log.v(LOG_TAG, "Word at index 6: " + words[6]);
-        Log.v(LOG_TAG, "Word at index 7: " + words[7]);
-        Log.v(LOG_TAG, "Word at index 8: " + words[8]);
-        Log.v(LOG_TAG, "Word at index 9: " + words[9]);
-        */
-
-        //Create an ArrayList of words
         ArrayList<Word> words = new ArrayList<>();
 
         words.add(new Word("one", "lutti"));
@@ -60,15 +32,6 @@ public class NumbersActivity extends AppCompatActivity {
         WordAdapter itemsAdapter = new WordAdapter(this, words);
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
-
-        /*
-        for (int index = 0; index < words.size(); index++) {
-
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }*/
-
     }
 }
 
