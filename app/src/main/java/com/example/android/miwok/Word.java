5b2919d40758f27a884bf8a7a5e4e9d208a55e59
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-07-04 20:02:08 +0100

    All categories now have working audio

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,28 +7,83 @@ public class Word {
 
     private int mImageID;
 
-    Word(String defaultWord, String translatedWord) {
-        this.mDefaultWord = defaultWord;
-        this.mTranslatedWord = translatedWord;
+    private int mAudioID;
 
+
+    /**
+     * Create a new Word object with a Image and Audio file
+     *
+     * @param mDefaultWord    is the word in a language that the user is already familiar with (such as English)
+     * @param mTranslatedWord is the word in a language that the user is trying to learn
+     * @param mImageID        is the location of the image (R.drawable.whatever)
+     * @param mAudioID        is the lcoation of the audio file (R.raw.whatever)
+     */
+    public Word(String mDefaultWord, String mTranslatedWord, int mImageID, int mAudioID) {
+        this.mDefaultWord = mDefaultWord;
+        this.mTranslatedWord = mTranslatedWord;
+        this.mImageID = mImageID;
+        this.mAudioID = mAudioID;
     }
 
-    Word(String defaultWord, String translatedWord, int imageID) {
-        this.mDefaultWord = defaultWord;
-        this.mTranslatedWord = translatedWord;
-        this.mImageID = imageID;
+    /**
+     * Create a new Word object with a Audio file
+     *
+     * @param mDefaultWord    is the word in a language that the user is already familiar with (such as English)
+     * @param mTranslatedWord is the word in a language that the user is trying to learn
+     * @param mAudioID        is the lcoation of the audio file (R.raw.whatever)
+     */
+    Word(String mDefaultWord, String mTranslatedWord, int mAudioID) {
+        this.mDefaultWord = mDefaultWord;
+        this.mTranslatedWord = mTranslatedWord;
+        this.mAudioID = mAudioID;
     }
 
+    /**
+     * Create a new Word object
+     *
+     * @param mDefaultWord    is the word in a language that the user is already familiar with (such as English)
+     * @param mTranslatedWord is the word in a language that the user is trying to learn
+     */
+    public Word(String mDefaultWord, String mTranslatedWord) {
+        this.mDefaultWord = mDefaultWord;
+        this.mTranslatedWord = mTranslatedWord;
+    }
+
+    /**
+     * Get the location of where the audio file for the current word is stored.
+     *
+     * @return R.raw.whatever
+     */
+    public int getAudioID() {
+        return mAudioID;
+    }
+
+    /**
+     * Get the location of where the image file for the current word is stored.
+     *
+     * @return R.drawable.whatever
+     */
+    public int getImageID() {
+        return mImageID;
+    }
+
+    /**
+     * Get the String of the current word in the default language
+     *
+     * @return "One"
+     */
     public String getWordDefault() {
         return mDefaultWord;
     }
 
+    /**
+     * Get the String of the current word in the language you're trying to learn.
+     *
+     * @return "lutti"
+     */
     public String getWordTranslated() {
         return mTranslatedWord;
     }
 
-    public int getImageID() {
-        return mImageID;
-    }
 
 }
