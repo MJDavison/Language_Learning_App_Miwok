6ae69d427808bb523bf1938d021c12d1b33c1442
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-07-05 19:40:27 +0100

    Transferred NumbersActivity functionality to NumbersFragment.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,133 +1,32 @@
 package com.example.android.miwok;
 
-import android.content.Context;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = NumbersActivity.class.getSimpleName();
-    AudioManager mAudioManager;
-    private MediaPlayer mMediaPlayer;
-    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-            //Transient Loss means we've only lost it for a short time- it will be returning.
-            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT) {
-                mMediaPlayer.pause();
-                mMediaPlayer.seekTo(0);
-            }
-            //Gain means we have been given the AudioFocus, so we can play the sound without worries.
-            else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                mMediaPlayer.start();
-            }
-            //Loss means we've completely lost AudioFocus- It's not being returned to us until we request it again.
-            else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                releaseMediaPlayer();
-            }
 
-        }
-    };
-    MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mp) {
-            releaseMediaPlayer();
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.container, new NumbersFragment())
+                .commit();
 
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-        final ArrayList<Word> words = new ArrayList<>();
-
-
-        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
-        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
-        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
-        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
-        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
-        words.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
-        words.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
-
-        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_numbers);
-        ListView listView = findViewById(R.id.list);
-        listView.setAdapter(itemsAdapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Get the {@link Word} object at the given position the user clicked on
-                Word word = words.get(position);
-
-                //Release the media player if it currently exists-
-                // We are about to play a different audio file
-                releaseMediaPlayer();
-
-                //Request AudioFocus with the stream type of Music for the duration of Gain_Transient. This means we gain audio focus for a short time.
-                // This is okay, since our sound clips are very short.
-                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-
-                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-                    // We were given AudioFocus for a short time.
-
-                    // Create and setup the {@link MediaPlayer} for the audio resource associated
-                    // with the current word
-                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioID());
-                    // Start the audio file
-                    mMediaPlayer.start();
-                    // Setup a listener on the media player, so that we can stop and release the
-                    // media player once the sounds have finished playing
-                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                }
-            }
-        });
     }
 
     /**
      * Clean up the media player by releasing its resources.
      */
-    private void releaseMediaPlayer() {
-        // If the media player is not null, then it may be currently playing a sound.
-        if (mMediaPlayer != null) {
-            // Regardless of the current state of the media player, release its resources
-            // because we no longer need it.
-            mMediaPlayer.release();
 
-            // Set the media player back to null. For our code, we've decided that
-            // setting the media player to null is an easy way to tell that the media player
-            // is not configured to play an audio file at the moment.
-            mMediaPlayer = null;
-
-
-            //Since we're releasing the MediaPlayer, which is what uses the AudioFocus,
-            // we obviously don't need audio focus anymore, so abandon it
-            // regardless of whether we have it or not.
-            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-        }
-    }
 
     @Override
     protected void onStop() {
         super.onStop();
-        releaseMediaPlayer();
+
 
     }
 }
